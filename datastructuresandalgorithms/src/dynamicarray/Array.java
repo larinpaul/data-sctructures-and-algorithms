9025b895b12d098bc98cf65492c18b87fbd7ea40
larinpaul <www.larinpavel@mail.ru>
2023-11-18 23:52:30 +0500

    First commit

---
+++ datastructuresandalgorithms/src/dynamicarray/Array.java
@@ -0,0 +1,27 @@
+package dynamicarray;
+
+import java.util.Iterator;
+
+public class Array<T> implements Iterable<T> {
+	
+	private T [] arr;
+	private int len = 0; // length user thinks array is
+	private int capacity = 0; // Actual array size
+	
+	public Array() { this(16); }
+	
+	public Array(int capacity) {
+		if (capacity < 0) throw new IllegalArgumentException("Illegal capacity" + capacity);
+		this.capacity = capacity;
+		arr = (T[]) new Object[capacity];
+	}
+
+	
+	
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ datastructuresandalgorithms/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module datastructuresandalgorithms {
+}
